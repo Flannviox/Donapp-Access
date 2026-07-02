d27a2f832f43e32c5c956c33126abba56e061f36
Ale152277 <gsjoaquin22@gmail.com>
2026-07-01 23:07:18 -0500

    .

--- app/src/main/java/com/grupo3/donapp_access/data/model/NotificacionesAlerta.kt
+++
@@ -1,8 +0,0 @@
-package com.grupo3.donapp_access.data.model
-
-data class NotificacionesAlerta(
-    val fechaHora: String,
-    val mensaje: String,
-    val tiendaId: String,
-    val distancia: Double
-)
